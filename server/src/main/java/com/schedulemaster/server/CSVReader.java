fac5e081eda014a6dbe834aa99d210495fbe746d
lalaalal <bontin0128@gmail.com>
2022-10-17 00:18:40 +0900

    Make CSVReader recognize ""

--- server/src/main/java/com/schedulemaster/server/CSVReader.java
+++ server/src/main/java/com/schedulemaster/server/CSVReader.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CSVReader implements AutoCloseable {
 
@@ -15,10 +17,22 @@ public class CSVReader implements AutoCloseable {
 
     public String[] read() throws IOException {
         String line = reader.readLine();
+        if (line == null)
+            return null;
 
-        if (line != null)
-            return line.split(",");
-        return null;
+        String[] tuple = line.split("\"");
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < tuple.length; i++) {
+            if (i % 2 == 0) {
+                if (tuple[i].charAt(0) == ',')
+                    tuple[i] = tuple[i].substring(1);
+                String[] parts = tuple[i].split(",");
+                result.addAll(Arrays.stream(parts).toList());
+            } else {
+                result.add(tuple[i]);
+            }
+        }
+        return result.toArray(new String[0]);
     }
 
     @Override
