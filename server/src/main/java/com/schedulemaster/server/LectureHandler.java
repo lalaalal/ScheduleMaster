c0149c32d97e71f0dc0131cd4d3419a9b046adb4
lalaalal <bontin0128@gmail.com>
2022-10-17 00:30:35 +0900

    Basic LectureHandler

---
+++ server/src/main/java/com/schedulemaster/server/LectureHandler.java
@@ -0,0 +1,44 @@
+package com.schedulemaster.server;
+
+import com.schedulemaster.misc.LinkedList;
+import com.schedulemaster.model.Lecture;
+import com.schedulemaster.model.LectureTime;
+
+import java.io.IOException;
+
+public class LectureHandler {
+    private final LinkedList<Lecture> lectures = new LinkedList<>();
+
+    public LectureHandler(String lectureDataPath) {
+        try (CSVReader csvReader = new CSVReader(lectureDataPath)) {
+            String[] tuple = csvReader.read();
+            while ((tuple = csvReader.read()) != null) {
+                Lecture lecture = Lecture.createLecture(tuple);
+                Lecture sameLecture = findLecture(lecture.lectureNum);
+                if (sameLecture == null) {
+                    lectures.push(lecture);
+                } else {
+                    LinkedList<LectureTime.TimeSet> timeSets = lecture.time.getTimeSets();
+                    sameLecture.time.addTimeSets(timeSets);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Lecture findLecture(String lectureNumber) {
+        for (Lecture lecture : lectures) {
+            if (lecture.lectureNum.equals(lectureNumber)) {
+                return lecture;
+            }
+        }
+
+        return null;
+    }
+
+    public LinkedList<Lecture> getLectures() {
+        return lectures;
+    }
+}
