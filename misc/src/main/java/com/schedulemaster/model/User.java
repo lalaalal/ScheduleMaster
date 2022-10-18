3be2dc8293ed4ebbe57c3dfc5b30a301f85f162f
lalaalal <bontin0128@gmail.com>
2022-10-18 15:41:28 +0900

    Change User.pw to User.hashedPassword

--- misc/src/main/java/com/schedulemaster/model/User.java
+++ misc/src/main/java/com/schedulemaster/model/User.java
@@ -1,27 +1,25 @@
 package com.schedulemaster.model;
 
 import com.schedulemaster.misc.LinkedList;
-import com.schedulemaster.util.SHA256;
 
 import java.io.Serializable;
-import java.security.NoSuchAlgorithmException;
 
 public class User implements Serializable {
-    public static final long serialVersionUID = 2L;
+    public static final long serialVersionUID = 11L;
 
-    public String id;
-    private String pw;
+    public final String id;
+    private final String hashedPassword;
 
     public LinkedList<Lecture> selectedLectures = new LinkedList<>(); // 책가방 강의
     public LinkedList<Lecture> enrolledLectures = new LinkedList<>(); // 신청 완료된 강의
 
-    public User(String id, String pw) {
+    public User(String id, String hashedPassword) {
         this.id = id;
-        this.pw = SHA256.encrypt(pw);
+        this.hashedPassword = hashedPassword;
     }
 
     public boolean verifyPassword(String hashedPassword) {
-        return pw.equals(hashedPassword);
+        return this.hashedPassword.equals(hashedPassword);
     }
 
     public void enrollLecture(Lecture lecture) {
@@ -38,5 +36,31 @@ public class User implements Serializable {
 
     public void unselectLecture(Lecture lecture) {
         selectedLectures.remove(lecture);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", pw='" + hashedPassword + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!id.equals(user.id)) return false;
+        return hashedPassword.equals(user.hashedPassword);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + hashedPassword.hashCode();
+        return result;
     }
 }
