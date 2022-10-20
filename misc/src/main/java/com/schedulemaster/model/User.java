b4ee5f7627c8057cce7e1c9fda9500b334720d4c
lalaalal <bontin0128@gmail.com>
2022-10-21 02:29:54 +0900

    Add Priority, Update User

--- misc/src/main/java/com/schedulemaster/model/User.java
+++ misc/src/main/java/com/schedulemaster/model/User.java
@@ -1,17 +1,20 @@
 package com.schedulemaster.model;
 
+import com.schedulemaster.misc.Heap;
 import com.schedulemaster.misc.LinkedList;
 
 import java.io.Serializable;
 
 public class User implements Serializable {
-    public static final long serialVersionUID = 11L;
+    public static final long serialVersionUID = 12L;
 
     public final String id;
     private final String hashedPassword;
 
-    public LinkedList<Lecture> selectedLectures = new LinkedList<>(); // 책가방 강의
-    public LinkedList<Lecture> enrolledLectures = new LinkedList<>(); // 신청 완료된 강의
+    public final LinkedList<Lecture> selectedLectures = new LinkedList<>(); // 책가방 강의
+    public final LinkedList<Lecture> enrolledLectures = new LinkedList<>(); // 신청 완료된 강의
+    public final Heap<Priority> priorities = new Heap<>(Heap.Comparator.maxHeapInt(Priority::priority));
+    public final LectureTime unwantedTime = new LectureTime();
 
     public User(String id, String hashedPassword) {
         this.id = id;
@@ -36,6 +39,10 @@ public class User implements Serializable {
 
     public void unselectLecture(Lecture lecture) {
         selectedLectures.remove(lecture);
+    }
+
+    public void addUnwantedTime(LectureTime.TimeSet timeSet) {
+        unwantedTime.addTimeSet(timeSet);
     }
 
     @Override
