db3367afb84201f280c2a39df5f5d9249d2b21c8
lalaalal <bontin0128@gmail.com>
2022-10-21 14:48:33 +0900

    MagicController

--- app/src/main/java/com/schedulemaster/app/controller/MagicController.java
+++ app/src/main/java/com/schedulemaster/app/controller/MagicController.java
@@ -1,35 +1,79 @@
 package com.schedulemaster.app.controller;
 
 import com.schedulemaster.app.model.LectureGroup;
+import com.schedulemaster.app.model.Priority;
 import com.schedulemaster.app.model.Schedule;
+import com.schedulemaster.misc.Hash;
+import com.schedulemaster.misc.Heap;
 import com.schedulemaster.misc.LinkedList;
 import com.schedulemaster.model.Lecture;
-import com.schedulemaster.model.User;
+
+import java.util.Iterator;
 
 public class MagicController {
 
-    private User user;
+    private final UserController userController;
 
     private final LinkedList<Schedule> schedules = new LinkedList<>();
     private final LinkedList<LectureGroup> lectureGroups = new LinkedList<>();
+    private final Hash<Lecture, Integer> priorities = new Hash<>();
 
-    public MagicController() {
-
+    public MagicController(UserController userController) {
+        this.userController = userController;
     }
 
     public void addGroup() {
         lectureGroups.push(new LectureGroup());
     }
 
-    public void addLecture(int groupNumber, Lecture lecture) {
+    public void addLecture(int groupNumber, Lecture lecture, int priority) {
         if (lectureGroups.getLength() <= groupNumber)
             throw new IndexOutOfBoundsException();
         LectureGroup group = lectureGroups.at(groupNumber);
+        priorities.put(lecture, priority);
         group.push(lecture);
     }
 
-    public void magic() {
+    public void changePriority(Lecture lecture, int priority) {
+        priorities.set(lecture, priority);
+    }
 
+    public void magic() {
+        schedules.clear();
+        createSchedules(lectureGroups.iterator(), null, new Schedule());
+    }
+
+    private void createSchedules(Iterator<LectureGroup> iterator, LectureGroup curr, Schedule schedule) {
+        if (curr == null && iterator.hasNext())
+            curr = iterator.next();
+        if (curr == null)
+            return;
+
+        LectureGroup next = null;
+        if (iterator.hasNext())
+            next = iterator.next();
+
+        Heap<Priority> priorityHeap = curr.createHeap(priorities);
+        Schedule clone = schedule.copy();
+        if (!priorityHeap.isEmpty()) {
+            Lecture lecture = priorityHeap.pop().lecture();
+
+            schedule.addLecture(lecture);
+            if (next == null)
+                schedules.push(schedule);
+            else
+                createSchedules(iterator, next, schedule);
+        }
+        while (!priorityHeap.isEmpty()) {
+            Lecture lecture = priorityHeap.pop().lecture();
+
+            Schedule newSchedule = clone.copy();
+            newSchedule.addLecture(lecture);
+            if (next == null)
+                schedules.push(newSchedule);
+            else
+                createSchedules(iterator, next, newSchedule);
+        }
     }
 
     public Schedule[] getSchedules() {
--- app/src/main/java/com/schedulemaster/app/model/Schedule.java
+++ app/src/main/java/com/schedulemaster/app/model/Schedule.java
@@ -9,13 +9,14 @@ import com.schedulemaster.model.Lecture;
 public class Schedule {
     private final LinkedList<Lecture> lectures = new LinkedList<>();
 
-    public void addLecture(Lecture lecture) {
+    public boolean addLecture(Lecture lecture) {
         for (Lecture element : lectures) {
             if (element.time.conflictWith(lecture.time))
-                return;
+                return false;
         }
 
         lectures.push(lecture);
+        return true;
     }
 
     public void removeLecture(Lecture lecture) {
@@ -24,5 +25,19 @@ public class Schedule {
 
     public LinkedList<Lecture> getLectures() {
         return lectures;
+    }
+
+    public Schedule copy() {
+        Schedule clone = new Schedule();
+        clone.lectures.addAll(this.lectures);
+
+        return clone;
+    }
+
+    @Override
+    public String toString() {
+        return "Schedule{" +
+                "lectures=" + lectures +
+                '}';
     }
 }
