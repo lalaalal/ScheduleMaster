5748b4ba120569d74131c30e69ab9a2d4778b305
lalaalal <bontin0128@gmail.com>
2022-10-21 11:58:47 +0900

    Add MagicController

---
+++ app/src/main/java/com/schedulemaster/app/model/LectureGroup.java
@@ -0,0 +1,8 @@
+package com.schedulemaster.app.model;
+
+import com.schedulemaster.misc.LinkedList;
+import com.schedulemaster.model.Lecture;
+
+public class LectureGroup extends LinkedList<Lecture> {
+
+}
