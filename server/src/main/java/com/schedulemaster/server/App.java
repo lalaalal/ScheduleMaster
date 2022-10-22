d49d7b9863d87dd4bd219c6d124e642cadb56a00
lalaalal <bontin0128@gmail.com>
2022-10-22 15:33:37 +0900

    Server App

--- server/src/main/java/com/schedulemaster/server/App.java
+++ server/src/main/java/com/schedulemaster/server/App.java
@@ -1,7 +1,54 @@
 package com.schedulemaster.server;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class App {
+    public static final String DEFAULT_LECTURE_FILE_PATH = "lectures";
+    public static final String DEFAULT_USER_FILE_PATH = "users";
+
+    private final LectureHandler lectureHandler;
+    private final UserHandler userHandler;
+
+    Logger logger = Logger.getInstance();
+
     public static void main(String[] args) {
-        System.out.println("Hello, Server!");
+        App app = new App(args);
+        app.startServer();
+    }
+    public App(String[] args) {
+        logger.addOutputStream(System.out);
+
+        String lectureFilePath = DEFAULT_LECTURE_FILE_PATH;
+        String userFilePath = DEFAULT_USER_FILE_PATH;
+
+        if (args.length >= 1)
+            lectureFilePath = args[0];
+        if (args.length >= 2)
+            userFilePath = args[1];
+
+        lectureHandler = new LectureHandler(lectureFilePath);
+        userHandler = new UserHandler(userFilePath);
+    }
+
+    public void startServer() {
+        try (Server server = new Server(lectureHandler, userHandler);
+             Scanner scanner = new Scanner(System.in)) {
+            Thread serverThread = new Thread(server);
+            serverThread.start();
+
+            String command = "";
+            while (!command.equals("exit")) {
+                command = scanner.next();
+                handleCommand(command, scanner);
+            }
+
+        } catch (IOException e) {
+            logger.log(e.getMessage(), Logger.ERROR);
+        }
+    }
+
+    private void handleCommand(String command, Scanner scanner) {
+        
     }
 }
--- server/src/main/java/com/schedulemaster/server/Server.java
+++ server/src/main/java/com/schedulemaster/server/Server.java
@@ -12,22 +12,24 @@ public class Server implements AutoCloseable, Runnable {
     private final UserHandler userHandler;
     private final Logger logger = Logger.getInstance();
 
+    private boolean run = true;
+
     public Server(LectureHandler lectureHandler, UserHandler userHandler) throws IOException {
         serverSocket = new ServerSocket(PORT);
         this.lectureHandler = lectureHandler;
         this.userHandler = userHandler;
-        logger.addOutputStream(System.out);
     }
 
     @Override
     public void close() throws IOException {
+        run = false;
         serverSocket.close();
     }
 
     @Override
     public void run() {
         logger.log("Starting server", Logger.INFO);
-        while (true) {
+        while (run) {
             try {
                 Socket client = serverSocket.accept();
                 logger.log("New connection from " + client.getInetAddress(), Logger.INFO);
