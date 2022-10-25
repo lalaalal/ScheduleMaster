dd09b4d0cb84a9fa2c1888b59a2a43b185d7ab7e
lalaalal <bontin0128@gmail.com>
2022-10-25 23:41:25 +0900

    Implement TimeTableForm.updateView

--- app/src/main/java/com/schedulemaster/app/view/TimeTableForm.java
+++ app/src/main/java/com/schedulemaster/app/view/TimeTableForm.java
@@ -2,27 +2,50 @@ package com.schedulemaster.app.view;
 
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.schedulemaster.misc.Hash;
+import com.schedulemaster.misc.LinkedList;
+import com.schedulemaster.model.Lecture;
+import com.schedulemaster.model.LectureTime;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
 
 public class TimeTableForm extends LectureView {
+    private record Position(int row, int column) {
+    }
+
+    private final Hash<Position, Color> colors = new Hash<>();
+
     private JTable timeTable;
     private JPanel panel;
 
     private static final String[] HEADER = {"", "월", "화", "수", "목", "금"};
     private static final String[][] RAW_DATA = {
-            {"1교시", "", "", "", "", ""},
-            {"2교시", "", "", "", "", ""},
-            {"3교시", "", "", "", "", ""},
-            {"4교시", "", "", "", "", ""},
-            {"5교시", "", "", "", "", ""},
-            {"6교시", "", "", "", "", ""},
-            {"7교시", "", "", "", "", ""},
-            {"8교시", "", "", "", "", ""},
-            {"9교시", "", "", "", "", ""},
-            {"10교시", "", "", "", "", ""}
+            {"1교시", "", "", "", "", ""}, // 9
+            {"2교시", "", "", "", "", ""}, // 10
+            {"3교시", "", "", "", "", ""}, // 11
+            {"4교시", "", "", "", "", ""}, // 12
+            {"5교시", "", "", "", "", ""}, // 13
+            {"6교시", "", "", "", "", ""}, // 14
+            {"7교시", "", "", "", "", ""}, // 15
+            {"8교시", "", "", "", "", ""}, // 16
+            {"9교시", "", "", "", "", ""}, // 17
+            {"10교시", "", "", "", "", ""} // 18
     };
+
+    private static final int FIRST_CLASS_HOUR = 9;
+    private static final int LAST_CLASS_HOUR = 19;
+
+    private static final LectureTime.Time[] CLASS_TIME = new LectureTime.Time[10];
+
+    private static final Color[] COLORS = {Color.BLUE, Color.RED, Color.GREEN, Color.LIGHT_GRAY};
+
+    static {
+        for (int time = FIRST_CLASS_HOUR, index = 0; time < LAST_CLASS_HOUR; time++, index++) {
+            CLASS_TIME[index] = new LectureTime.Time(time, 0);
+        }
+    }
 
     public TimeTableForm() {
 
@@ -30,7 +53,54 @@ public class TimeTableForm extends LectureView {
 
     @Override
     public void updateView() {
+        colors.clear();
+        int colorIndex = 0;
+        for (Lecture lecture : lectures) {
+            setClassTimes(lecture.getTime(), colorIndex);
+            colorIndex += 1;
+        }
+    }
 
+    private void setClassTimes(LectureTime lectureTime, int colorIndex) {
+        LinkedList<LectureTime.TimeSet> timeSets = lectureTime.getTimeSets();
+
+        for (LectureTime.TimeSet timeSet : timeSets) {
+            int[] rows = calcClassTimes(timeSet);
+            for (int row : rows) {
+                Position position = new Position(row, timeSet.dayOfWeek() + 1);
+                colors.put(position, COLORS[colorIndex % COLORS.length]);
+            }
+
+        }
+
+    }
+
+    public int[] calcClassTimes(LectureTime.TimeSet timeSet) {
+        int startClassIndex = 0;
+        if (timeSet.start().isAfter(CLASS_TIME[9]))
+            return new int[0];
+        LectureTime.TimeSet compare = new LectureTime.TimeSet(timeSet.dayOfWeek(), CLASS_TIME[startClassIndex], CLASS_TIME[9]);
+        while (compare.include(timeSet)) {
+            startClassIndex += 1;
+            compare = new LectureTime.TimeSet(timeSet.dayOfWeek(), CLASS_TIME[startClassIndex], CLASS_TIME[9]);
+        }
+        if (startClassIndex > 0)
+            startClassIndex -= 1;
+
+        int endClassIndex = 9;
+        compare = new LectureTime.TimeSet(timeSet.dayOfWeek(), CLASS_TIME[startClassIndex], CLASS_TIME[endClassIndex]);
+        while (endClassIndex > startClassIndex && compare.include(timeSet)) {
+            endClassIndex -= 1;
+            compare = new LectureTime.TimeSet(timeSet.dayOfWeek(), CLASS_TIME[startClassIndex], CLASS_TIME[endClassIndex]);
+        }
+
+        int length = endClassIndex - startClassIndex + 1;
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = startClassIndex + i;
+        }
+
+        return result;
     }
 
     public JPanel getPanel() {
@@ -41,6 +111,20 @@ public class TimeTableForm extends LectureView {
         timeTable = new JTable(RAW_DATA, HEADER);
         timeTable.getTableHeader().setReorderingAllowed(false);
         timeTable.setEnabled(false);
+        timeTable.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                JComponent component = (JComponent) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                component.setBorder(BorderFactory.createMatteBorder(1, 1, 0, 0, Color.LIGHT_GRAY));
+                Position position = new Position(row, column);
+                Color color = colors.get(position);
+                if (color != null)
+                    component.setBackground(color);
+                else
+                    component.setBackground(Color.WHITE);
+                return component;
+            }
+        });
     }
 
     {
---
+++ app/src/test/java/com/schedulemaster/app/TimeTableFormTest.java
@@ -0,0 +1,56 @@
+package com.schedulemaster.app;
+
+import com.schedulemaster.app.view.TimeTableForm;
+import com.schedulemaster.model.Lecture;
+import com.schedulemaster.model.LectureTime;
+import mdlaf.MaterialLookAndFeel;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+import javax.swing.plaf.ColorUIResource;
+import java.awt.*;
+
+public class TimeTableFormTest {
+    @Test
+    public void testCalcClassTime(){
+        TimeTableForm timeTableForm = new TimeTableForm();
+        LectureTime.Time start = LectureTime.Time.parseTime("13:00");
+        LectureTime.Time end = LectureTime.Time.parseTime("14:50");
+        LectureTime.TimeSet timeSet = new LectureTime.TimeSet(0, start, end);
+        int[] classTimes = timeTableForm.calcClassTimes(timeSet);
+        for (int classTime : classTimes) {
+            System.out.println(classTime);
+        }
+    }
+
+    @Test
+    public void testUpdateView() throws InterruptedException, UnsupportedLookAndFeelException {
+        UIManager.setLookAndFeel(new MaterialLookAndFeel());
+        UIManager.put("Table.gridColor", new ColorUIResource(Color.gray));
+        JFrame frame = new JFrame();
+        frame.setSize(500, 400);
+        TimeTableForm timeTableForm = new TimeTableForm();
+        frame.setContentPane(timeTableForm.getPanel());
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Lecture lecture = new Lecture();
+        LectureTime.Time start = LectureTime.Time.parseTime("13:00");
+        LectureTime.Time end = LectureTime.Time.parseTime("14:50");
+        LectureTime.TimeSet timeSet = new LectureTime.TimeSet(0, start, end);
+        lecture.time = new LectureTime();
+        lecture.time.addTimeSet(timeSet);
+
+        Lecture lecture2 = new Lecture();
+        start = LectureTime.Time.parseTime("13:00");
+        end = LectureTime.Time.parseTime("14:50");
+        timeSet = new LectureTime.TimeSet(2, start, end);
+        lecture2.time = new LectureTime();
+        lecture2.time.addTimeSet(timeSet);
+
+        timeTableForm.addLecture(lecture);
+        timeTableForm.addLecture(lecture2);
+        timeTableForm.updateView();
+
+        Thread.sleep(15000);
+    }
+}
